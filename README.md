2957aad0116f80e0d848b759de28897b8fb97019
Jimmy Andersson <jimmyandersson89@gmail.com>
2020-09-30 15:50:03 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,24 +1,8 @@
 # todo-vue3
 
+This todo application is built with the new composition API in Vue 3. For styling I'm using materialize css.
+
 ## Project setup
 ```
 npm install
 ```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1 class="center-align">Todo App</h1>
+    <form @submit.prevent="addTodo">
+      <div class="input-field col">
+        <input type="text" id="todo" v-model="text" />
+        <label for="todo">Todo</label>
+      </div>
+    </form>
+    <div v-if="todos" class="">
+      <ul class="collection">
+        <li
+          :class="{ done: todo.done }"
+          class="collection-item"
+          v-for="(todo, index) in todos"
+          :key="todo.id"
+        >
+          <div>
+            <span @click="toggleDone(index)">{{ todo.content }}</span>
+
+            <a href="#!" class="secondary-content" @click="deleteTodo(index)"
+              ><i class="material-icons">delete</i></a
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { reactive, ref } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  setup() {
+    const todos = ref([]);
+    const text = ref('')
+    
+    function addTodo() {
+      const currentTodo = reactive({
+      id: Date.now(),
+      done: false,
+      content: text.value,
+      })
+      todos.value.push(currentTodo)
+      text.value = ''
+    }
+    function toggleDone(index) {
+      todos.value[index].done = !todos.value[index].done;
+    }
+    function deleteTodo(index) {
+      todos.value.splice(index, 1);
+    }
+    return {
+      deleteTodo,
+      toggleDone,
+      text,
+      todos,
+      addTodo,
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done {
+  text-decoration: line-through;
 }
 </style>
